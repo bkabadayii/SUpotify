fdf86664429dec5ed86471e5080016f9aa537a05
Deniz <ozdemirdeniz@sabanciuniv.edu>
2023-10-28 13:33:56 +0300

    first commit

---
+++ Mobile/Demo Swift/Demo_SwiftApp.swift
@@ -0,0 +1,18 @@
+//
+//  Demo_SwiftApp.swift
+//  Demo Swift
+//
+//  Created by Deniz Özdemir on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
