dd97aee1dc3699c807c72c608cf29103cdb35733
Deniz <ozdemirdeniz@sabanciuniv.edu>
2023-10-31 14:01:03 +0300

    created login and signup page

---
+++ Mobile/SUPotify Mobile/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  SUPotify Mobile App
+//
+//  Created by Deniz Özdemir on 10/30/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ Mobile/SUPotify Mobile/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  SUPotify Mobile App
+//
+//  Created by Deniz Özdemir on 10/31/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
