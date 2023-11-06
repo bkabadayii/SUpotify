99c35945b32eb9f69568e7ff764f2e0e8341ac09
Baturalp Kabadayi <baturalparslann@hotmail.com>
2023-11-07 01:42:07 +0300

    Fix browser request denial bug

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 const { MONGO_URL, PORT } = process.env;
@@ -23,6 +25,15 @@ mongoose
     .catch((err) => console.error(err));
 
 // ----- Middlewares -----
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+app.use(cookieParser());
+
 app.use(express.json());
 // ----- Middlewares -----
 
