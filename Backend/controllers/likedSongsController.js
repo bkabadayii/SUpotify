d51fecdbee2e48a09b84c48620b87b9059c9ab28
Baturalp Kabadayi <baturalparslann@hotmail.com>
2023-11-07 18:28:24 +0300

    Implement atomic operations for user liked songs * Add operation with error checks * Remove operation with error checks

--- Backend/controllers/likedSongsController.js
+++ Backend/controllers/likedSongsController.js
@@ -25,3 +25,91 @@ module.exports.createLikedSongsForUser = async (req, res) => {
         return res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+module.exports.addToUserLikedSongs = async (req, res) => {
+    try {
+        // Get username and songID from request body
+        const { username, songID } = req.body;
+        let existingUserLikedSongs = await LikedSongs.findOne({ username });
+
+        // If liked songs list is not initialized for user, throw error
+        if (!existingUserLikedSongs) {
+            return res.json({
+                message: "User liked songs does not exist!",
+                success: false,
+            });
+        }
+
+        // If user already liked the song with songID, throw error
+        const duplicate = existingUserLikedSongs.likedSongsList.find(
+            (existingSongID) => songID === existingSongID
+        );
+
+        if (duplicate) {
+            return res.json({
+                message: "Song ID already exists in user liked songs!",
+                success: false,
+            });
+        }
+
+        // If there are no errors, add songID to user's liked songs list
+        existingUserLikedSongs.likedSongsList.push(songID);
+        await existingUserLikedSongs.save();
+
+        res.status(201).json({
+            message: `Added songID: ${songID}, to liked songs of user: ${username}`,
+            success: true,
+        });
+    } catch (err) {
+        console.error(err);
+        return res
+            .status(500)
+            .json({ message: "Internal Server Error", success: false });
+    }
+};
+
+module.exports.removeFromUserLikedSongs = async (req, res) => {
+    try {
+        const { username, songID } = req.body;
+        let existingUserLikedSongs = await LikedSongs.findOne({ username });
+
+        // If liked songs list is not initialized for user, throw error
+        if (!existingUserLikedSongs) {
+            return res.json({
+                message: "User liked songs does not exist!",
+                success: false,
+            });
+        }
+
+        // If user have not liked the song with songID, throw error
+        const existingSongID = existingUserLikedSongs.likedSongsList.find(
+            (existingSongID) => songID === existingSongID
+        );
+
+        if (!existingSongID) {
+            return res.status(404).json({
+                message:
+                    "Cannot remove from liked songs because user has not liked the song!",
+                success: false,
+            });
+        }
+
+        // If there are no errors, remove the song from liked songs list
+        const updatedLikedSongs = await LikedSongs.findOneAndUpdate(
+            { username: username },
+            { $pull: { likedSongsList: songID } }, // Pulling item from the liked songs list
+            { new: true }
+        );
+
+        res.status(200).json({
+            message: "Song is removed from liked songs successfully",
+            updatedLikedSongs,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Internal Server Error",
+            success: false,
+        });
+    }
+};
--- Backend/routes/likedSongsRoute.js
+++ Backend/routes/likedSongsRoute.js
@@ -1,8 +1,13 @@
 const {
     createLikedSongsForUser,
+    addToUserLikedSongs,
+    removeFromUserLikedSongs,
 } = require("../controllers/likedSongsController");
+
 const router = require("express").Router();
 
 router.post("/createLikedSongsForUser", createLikedSongsForUser);
+router.post("/addToUserLikedSongs", addToUserLikedSongs);
+router.delete("/removeFromUserLikedSongs", removeFromUserLikedSongs);
 
 module.exports = router;
