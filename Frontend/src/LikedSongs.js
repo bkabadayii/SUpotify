8100202409f423039f0a1c927658cc254947fd98
gorkemdd <gorkemafsin@sabanciuniv.edu>
2023-11-11 17:18:22 +0300

    Add song(s) to Liked Songs

--- Frontend/src/LikedSongs.js
+++ Frontend/src/LikedSongs.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const LikedSongs = () => {
 
   const token = localStorage.getItem('token');
   const username = localStorage.getItem('username');
+  const [songID, setSongID] = useState('');
+  const [songIDList, setSongIDList] = useState([]);
 
   const createLikedSongs = async (token) => {
 
@@ -36,11 +38,93 @@ const LikedSongs = () => {
 
   };
 
+  const addToUserLikedSongs = async () => {
+    try {
+      const res = await axios.post(
+        'http://localhost:4000/api/likedSongs/addToUserLikedSongs',
+        {
+          songID: songID,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      // Handle the response as needed
+      console.log(res.data);
+
+      if (res.data.success === true)
+      {
+        alert('Added to Liked Songs!')
+      }
+      else
+      {
+        alert('Song already exists in Liked Songs.')
+      }
+
+    } catch (error) {
+      // Handle errors
+      console.error('Error adding song to user liked songs:', error);
+    }
+  };
+
+  const addManyToUserLikedSongs = async () => {
+    try {
+      const res = await axios.post(
+        'http://localhost:4000/api/likedSongs/addManyToUserLikedSongs',
+        {
+          songIDList: songIDList,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      // Handle the response as needed
+      console.log(res.data);
+
+      if (res.data.success === true)
+      {
+        alert('Added to Liked Songs!')
+      }
+      else
+      {
+        alert('Songs already exist in Liked Songs.')
+      }
+
+
+    } catch (error) {
+      // Handle errors
+      console.error('Error adding many songs to user liked songs:', error);
+    }
+  };
+
   return (
     <div>
-      <h2>Liked Songs of {username} </h2>
+      <h2>Add Songs </h2>
       <p></p>
       <button onClick = { () => createLikedSongs(token) } >Create Liked Songs</button>
+      <p></p>
+      <input
+        type = 'text'
+        placeholder = 'Enter Song ID'
+        value = { songID }
+        onChange={(e) => setSongID(e.target.value)}
+      />
+      <button onClick={addToUserLikedSongs}>Add to Liked Songs</button>
+      <p></p>
+      <textarea
+        placeholder="Enter Song IDs (comma-separated)"
+        value={songIDList.join(',')}
+        onChange={(e) => setSongIDList(e.target.value.split(','))}
+      />
+      <button onClick={addManyToUserLikedSongs}>Add Many to Liked Songs</button>
+      <p></p>
+      <h2>Liked Songs of {username} </h2>
     </div>
   );
 }
