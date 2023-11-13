bbde683fe47ea39f335ca40bed1a5db2df18b5a0
Cemal Efe Yılmaz <cefe@sabanciuniv.edu>
2023-11-14 02:11:13 +0300

    Update SpotifyAccessToken.js

--- Backend/util/SpotifyAccessToken.js
+++ Backend/util/SpotifyAccessToken.js
@@ -21,15 +21,18 @@ async function getAccessToken() {
         });
 
         const accessToken = response.data.access_token;
-
-        // Update the existing access token in .env file
-        fs.writeFileSync('.env', 
-        `MONGO_URL = mongodb+srv://admin:fmi9Qj5zCQ9077g2@supotifycluster.41kadml.mongodb.net/?retryWrites=true&w=majority
-PORT = 4000
-TOKEN_KEY = !1gorkemafsin1!
-SPOTIFY_CLIENT_ID =a3abf95fd683498f9d1df82b608b521d
-SPOTIFY_CLIENT_SECRET=2012fe02ce5246f8923d47b93455142b
-SPOTIFY_TOKEN=${accessToken}\n`);
+        const envFilePath = '.env';
+        const data = fs.readFileSync(envFilePath, 'utf8');
+        const lines = data.split('\n');
+        
+        //Delete old access token
+        lines.pop();
+        
+        //Append new access token
+        const newLine = `SPOTIFY_TOKEN=${accessToken}`;
+        lines.push(newLine);
+        const updatedData = lines.join('\n');
+        fs.writeFileSync(envFilePath, updatedData, 'utf8');
 
         return accessToken;
     } catch (error) {
