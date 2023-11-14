02de24e2e85a2bc01edd3ced4fd22c5506caeb03
Cemal Efe Yılmaz <cefe@sabanciuniv.edu>
2023-11-14 03:14:28 +0300

    Update search.js

--- Backend/Admin/search.js
+++ Backend/Admin/search.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 
-const accessToken =process.env.SPOTIFY_TOKEN;
+require("dotenv").config();
+
+const accessToken ="BQDIa-uQi_gICdqSiqfaNONG56lgbzGfNsKTesqgCE8LfhXjqoS0ebH8_xrtjv6VN7_g9Ga7lQvYSTbhLNfofYQvBh7tXc6TSu7KyATr5PblBhlOX5k";
 const songTitle = 'Keskin Bıçak'; // Replace with the title of the song
 const artistName ='İbrahim Tatlıses'; // Replace with the name of the artist
 
@@ -17,7 +19,7 @@ axios.get(`https://api.spotify.com/v1/search`, {
 })
   .then(response => {
     // Extract the song ID from the response
-    const songId = response.data.tracks.items; // Assuming you want the ID of the first match
+    const songId = response.data.tracks.items[0].id; // Assuming you want the ID of the first match
     console.log(`The ID of the song is: ${songId}`);
   })
   .catch(error => {
