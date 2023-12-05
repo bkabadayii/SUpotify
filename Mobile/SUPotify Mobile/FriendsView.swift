af86e39b204ee733c7c800d7e3439d82761908b3
Deniz <ozdemirdeniz@sabanciuniv.edu>
2023-12-05 15:37:15 +0300

    created friends view and integrated adding/removing friends

---
+++ Mobile/SUPotify Mobile/FriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsView.swift
+//  SUPotify Mobile App
+//
+//  Created by Deniz Özdemir on 12/5/23.
+//
+
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendsView()
+}
---
+++ Mobile/SUPotify Mobile/PlaylistGrid.swift
@@ -0,0 +1,18 @@
+//
+//  PlaylistGrid.swift
+//  SUPotify Mobile App
+//
+//  Created by Deniz Özdemir on 12/5/23.
+//
+
+import SwiftUI
+
+struct PlaylistGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaylistGrid()
+}
