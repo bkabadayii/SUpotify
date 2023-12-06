985c80d0164bad8e0eaf82800b99498d49a152e6
Baturalp Kabadayi <baturalparslann@hotmail.com>
2023-12-06 20:17:16 +0300

    Implement get content operations * Add getTrack, getAlbum, getArtist

--- Backend/routes/contentRoute.js
+++ Backend/routes/contentRoute.js
@@ -1,8 +1,13 @@
 const { addNewAlbum, addNewArtist } = require("../operations/addContent");
+const { getTrack, getAlbum, getArtist } = require("../operations/getContent");
 
 const router = require("express").Router();
 
 router.post("/addNewAlbum", addNewAlbum);
 router.post("/addNewArtist", addNewArtist);
+
+router.get("/getTrack/:trackID", getTrack);
+router.get("/getAlbum/:albumID", getAlbum);
+router.get("/getArtist/:artistID", getArtist);
 
 module.exports = router;
