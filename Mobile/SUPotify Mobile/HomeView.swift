7654feec4e84261c7ce3979df8e73c49892b628c
Deniz <ozdemirdeniz@sabanciuniv.edu>
2023-12-06 21:55:01 +0300

    Created playlist view, fixed search view and import bugs

---
+++ Mobile/SUPotify Mobile/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SUPotify Mobile App
+//
+//  Created by Deniz Özdemir on 12/6/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
