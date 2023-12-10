d3c4887b9638cfd2dd2db1cae278e53f51ff76b5
Deniz <ozdemirdeniz@sabanciuniv.edu>
2023-12-10 23:31:05 +0300

    added Foryou page adding to liked songs

--- Mobile/SUPotify Mobile/ForYouPage.swift
+++ Mobile/SUPotify Mobile/ForYouPage.swift
@@ -7,20 +7,20 @@
 import SwiftUI
 
 struct Track: Codable {
-   // let id: String
+  // let id: String
     let name: String
    // let popularity: Int
    // let durationMS: Int
     let album: Album
     let artists: [Artist]
-   // let spotifyID: String
+   let spotifyID: String
    // let spotifyURL: URL
   //  let previewURL: URL
   //  let v: Int
 }
 
 struct Album: Codable {
-  // let _id: String
+    let _id: String
     let name: String
     let imageURL: String
 }
@@ -51,9 +51,10 @@ struct ResponseTrackInfo: Codable {
 
 struct ForYouView: View {
     @State private var recommendedTracks: [Track] = []
-      @State private var isLoading = false
+      @State private var isLoading = true
       @State private var currentIdx: Int = 0
       @GestureState private var dragOffset: CGFloat = 0
+    @State var isRotated = false
 
       var body: some View {
           ZStack{
@@ -61,8 +62,31 @@ struct ForYouView: View {
               NavigationStack {
                   VStack {
                       if isLoading {
-                          ProgressView()
-                      } else {
+                              HStack{
+                                  Circle()
+                                      .strokeBorder(AngularGradient(gradient: Gradient(
+                                        colors: [.indigo, .blue, .purple, .orange, .red]),
+                                                                    center: .center,
+                                                                    angle: .zero),
+                                                    lineWidth: 15)
+                                      .rotationEffect(isRotated ? .zero : .degrees( 360))
+                                      .frame(maxWidth: 70, maxHeight: 70)
+                                      .onAppear {
+                                          withAnimation(Animation.spring(duration: 3)) {
+                                              isRotated.toggle()
+                                          }
+                                          withAnimation(Animation.linear(duration: 7).repeatForever(autoreverses: false)) {
+                                              isRotated.toggle()
+                                          }
+                                      }
+                                      .padding()
+                                  Text("We are bringing your recommendations for you!")
+                                      .font(.title)
+                                      .padding()
+                                      .foregroundColor(.white)
+                              }
+                          }
+                      else {
                           ZStack {
                               ForEach(recommendedTracks.indices, id: \.self) { index in
                                   Group {
@@ -89,7 +113,6 @@ struct ForYouView: View {
                                     }
                                 })
                           )
-                          ButtonsView()
                       }
                   }
                   .onAppear {
@@ -97,42 +120,41 @@ struct ForYouView: View {
                   }
               }}
        }
-
+    
     func fetchRecommendations() {
         let trackNum = 10
         let token = SessionManager.shared.token
         let url = URL(string: "http://localhost:4000/api/recommendation/recommendTrackFromFollowedUser/\(trackNum)")!
-
+        
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             DispatchQueue.main.async {
                 if let error = error {
                     print("Error: \(error.localizedDescription)")
                     return
                 }
-
+                
                 guard let data = data else {
                     print("No data received")
                     return
                 }
-
+                
                 do {
                     let response = try JSONDecoder().decode(Response.self, from: data)
                     print(response)
                     self.recommendedTracks = response.recommendations.map { $0.track }
                     print(response.recommendations)
-                    //let spotifyID = response.recommendations.map { $0.track.spotifyID }
-                    //print(spotifyID)
-
-
+                    self.isLoading = false
+                   
                 } catch {
                     print("Decoding Error: \(error.localizedDescription)")
                 }
             }
+            
         }.resume()
     }
 
@@ -144,65 +166,7 @@ struct ForYouView: View {
         .preferredColorScheme(.dark)
 }
 
-struct ButtonsView: View {
 
-    var body: some View {
-
-        HStack (spacing: 12){
-            Button(action: {
-                ShowSongInfo()
-            }, label: {
-                VStack (spacing: 8){
-                    Image(systemName: "music.note.list")
-                        .font(.title)
-                        .foregroundColor(.indigo)
-
-                }
-            })
-
-
-            Button(action: {
-                AddToLikedSongs()
-            }, label: {
-                VStack (spacing: 10){
-                    Image(systemName: "heart.fill")
-                        .font(.title)
-                        .foregroundColor(.indigo)
-
-
-                }
-            })
-
-            Button(action: {
-                AddToPlaylist()
-            }, label: {
-                VStack (spacing: 8){
-                    Image(systemName: "plus.circle.fill")
-                        .font(.title)
-                        .foregroundColor(.indigo)
-
-
-                }
-            })
-
-            Button(action: {
-                Share()
-            }, label: {
-                VStack (spacing: 8){
-                    Image(systemName: "square.and.arrow.up.fill")
-                        .font(.title)
-                        .foregroundColor(.indigo)
-
-
-                }
-            })
-        }
-        .frame(width: 300, height: 60)
-        .background(.black.opacity(0.30))
-        .cornerRadius(100)
-
-    }
-}
 
 class Host: UIHostingController<ContentView> {
     override var preferredStatusBarStyle: UIStatusBarStyle{
@@ -212,9 +176,11 @@ class Host: UIHostingController<ContentView> {
 
 struct TrackCardView: View {
     let track: Track
-
+    @State var isAdded: Bool = false
+    
     var body: some View {
-
+        VStack{
+            
             VStack(alignment: .center, spacing: 10) {
                 if let url = URL(string: track.album.imageURL) {
                     AsyncImage(url: url) { phase in
@@ -235,42 +201,161 @@ struct TrackCardView: View {
                         }
                     }
                 }
-
+                
                 Text(track.name)
                     .font(.headline)
                     .bold()
-
+                
                 ForEach(track.artists, id: \.name) { artist in
                     Text(artist.name)
                         .font(.subheadline)
                 }
-
+                
                 Text(track.album.name)
                     .font(.caption)
-
+                
             }
             .frame(width: 350, height: 550)
             .padding()
             .cornerRadius(20)
+            
+            HStack (spacing: 12){
+                Button(action: {
+                    ShowSongInfo()
+                }, label: {
+                    VStack (spacing: 8){
+                        Image(systemName: "music.note.list")
+                            .font(.title)
+                            .foregroundColor(.indigo)
+                        
+                    }
+                })
+                
+                
+                Button(action: {
+                    addToLikedSongs(trackId: track.spotifyID, albumId: track.album._id)
+                }, label: {
+                    VStack (spacing: 10){
+                        Image(systemName: "heart.fill")
+                            .font(.title)
+                            .foregroundColor(.indigo)
+                        
+                        
+                    }
+                }).alert(isPresented: $isAdded) {
+                    Alert(
+                        title: Text("Success"),
+                        message: Text("Recommended song added to liked songs successfully"),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
+
+                
+                Button(action: {
+                    AddToPlaylist()
+                }, label: {
+                    VStack (spacing: 8){
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title)
+                            .foregroundColor(.indigo)
+                    }
+                })
+                Button(action: {
+                    Share()
+                }, label: {
+                    VStack (spacing: 8){
+                        Image(systemName: "square.and.arrow.up.fill")
+                            .font(.title)
+                            .foregroundColor(.indigo)
+                    }
+                })
+            }
+            .frame(width: 300, height: 60)
+            .background(.black.opacity(0.30))
+            .cornerRadius(100)
         }
-
+        }
+    
+    
+    func addToLikedSongs(trackId: String, albumId: String) {
+        let token = SessionManager.shared.token
+        let urlString = "http://localhost:4000/api/likedContent/likeTrackBySpotifyID"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let body: [String: Any] = [
+            "spotifyID": trackId,
+            "albumSpotifyID": albumId
+        ]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: body)
+        } catch {
+            print("Error creating request body: \(error)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) {data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                print("HTTP Response Status: \(httpResponse.statusCode)")
+            }
+            
+            if let error = error {
+                print("Error adding track to liked songs: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            if let responseString = String(data: data, encoding: .utf8) {
+                print("Raw Response Data: \(responseString)")
+            }
+            
+            do {
+                if let responseData = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    if let success = responseData["success"] as? Bool {
+                        if success {
+                            // Track was successfully added to liked songs
+                            DispatchQueue.main.async {
+                                print("Recommended song added to liked songs successfully")
+                                isAdded = true
+                            }
+                        } else {
+                            // Handle the case where the server reports an error
+                            print("Error adding track to liked songs: \(responseData)")
+                        }
+                    } else {
+                        print("Unexpected response format")
+                    }
+                } else {
+                    print("Response data is not a dictionary")
+                }
+            } catch {
+                print("Error decoding response data: \(error)")
+            }
+        }.resume()
+    }
+        
 }
-
-
-
 
 func ShowSongInfo(){
-
+    
 }
 
-func AddToLikedSongs(){
 
-}
 
 func AddToPlaylist(){
-
+    
 }
 
 func Share(){
-
+    
 }
+
