bc2df8a43976377935a2deadac5b09e4ff6d014e
Baturalp Kabadayi <baturalparslann@hotmail.com>
2023-12-19 23:39:27 +0300

    Add deleteComment and switchCommentLikeStatus * deleteComment: Deletes a comment if it belongs to the user * switchCommentLikeStatus: Likes a comment if the user not liked, else removes user's like

--- Backend/routes/commentRoute.js
+++ Backend/routes/commentRoute.js
@@ -1,11 +1,15 @@
 const {
     getContentComments,
     commentContent,
+    deleteComment,
+    switchCommentLikeStatus,
 } = require("../controllers/commentsController");
 
 const router = require("express").Router();
 
 router.get("/getContentComments/:contentType/:relatedID", getContentComments);
 router.post("/commentContent", commentContent);
+router.delete("/deleteComment", deleteComment);
+router.post("/switchCommentLikeStatus", switchCommentLikeStatus);
 
 module.exports = router;
