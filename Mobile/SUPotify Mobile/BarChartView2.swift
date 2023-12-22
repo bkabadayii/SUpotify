85daf0f289f832d76ad637b95cec6a0d8e2e61bd
Alkım Özyüzer <alkimozyuzer@sabanciuniv.edu>
2023-12-22 16:09:15 +0300

    tracks bar chart v2

---
+++ Mobile/SUPotify Mobile/BarChartView2.swift
@@ -0,0 +1,18 @@
+//
+//  BarChartView2.swift
+//  SUPotify Mobile App
+//
+//  Created by Alkım Özyüzer on 22.12.2023.
+//
+
+import SwiftUI
+
+struct BarChartView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarChartView2()
+}
