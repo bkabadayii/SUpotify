7f79782e5fd3343cdcd294994271bed6a72c5dc8
Cemal Efe Yılmaz <cefe@sabanciuniv.edu>
2023-12-25 17:29:47 +0300

    Add rating based recommendation

--- Backend/routes/recommendationRoute.js
+++ Backend/routes/recommendationRoute.js
@@ -1,8 +1,11 @@
 const {
     recommendTrackFromFollowedUser,
+    recommendTrackFromUserRatings,
 } = require("../controllers/recommendationController");
 
 const router = require("express").Router();
 
 router.get("/recommendTrackFromFollowedUser/:trackNum",recommendTrackFromFollowedUser);
+router.get("/recommendTrackFromUserRatings",recommendTrackFromUserRatings);
+
 module.exports = router;
